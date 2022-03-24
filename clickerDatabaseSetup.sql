b13444f1e72b6030bb8fc0b016692e8e60570c5c
chewmuangg <yuch0014@e.ntu.edu.sg>
2022-03-24 18:18:56 +0800

    database + Select Servlet

---
+++ clickerDatabaseSetup.sql
@@ -0,0 +1,9 @@
+create database if not exists clicker;
+
+use clicker; 
+
+drop table if exists responses;
+
+create table responses (questionNo int, choice varchar(1));
+
+insert into responses values (1, 'A');
