033d13363e24b9662b5c5cab8b1bee993ee8ff36
Lam Yik Hong <lamyikhong123@yahoo.com.sg>
2022-03-24 15:48:45 +0800

    Main activity and question activity

--- app/src/main/java/com/example/qna/MainActivity.java
+++ app/src/main/java/com/example/qna/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.qna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtUsername;
+    private EditText txtRmCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * Callback when the user click the "GO" button
+     */
+    public void btnGoHandler(View view) {
+        // show the web page of the URL of the EditText
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtRmCode = (EditText) findViewById(R.id.txtRmCode);
+        if( TextUtils.isEmpty(txtUsername.getText())) {
+            txtUsername.setError("Username is required!");
+        } else if( TextUtils.isEmpty(txtRmCode.getText()) ) {
+            txtRmCode.setError("Room Code is required!");
+        } else {
+            Intent i = new Intent(this, question.class);
+            startActivity(i);
+        }
     }
 }
